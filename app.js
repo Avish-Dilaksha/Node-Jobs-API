4981ebadebf90b68d8afed6c882a2c5347ba527f
Avish-Dilaksha <adilaksha@gmail.com>
2023-03-05 19:42:23 +0530

    Created app.js
    Created app.js and configured for the initial state

---
+++ app.js
@@ -0,0 +1,24 @@
+require('dotenv').config()
+require('express-async-errors')
+
+const express = require('express')
+
+const app = express()
+
+
+app.use(express.json())
+
+const port = process.env.PORT || 3000
+
+const start = async () => {
+    try {
+        //await connectDB(process.env.MONGO_URL)
+        app.listen(port, () => {
+            console.log(`Server is listening on port ${port}...`);
+        })
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+start()
