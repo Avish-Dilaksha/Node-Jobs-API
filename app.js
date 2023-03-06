a44058019e1636c24ef577c479c2fda8af5215eb
Avish-Dilaksha <adilaksha@gmail.com>
2023-03-06 08:01:55 +0530

    Updated app.js
    Updated app.js by adding extra security packages and more

--- app.js
+++ app.js
@@ -2,6 +2,13 @@ require('dotenv').config()
 require('express-async-errors')
 
 const express = require('express')
+
+//extra security packages
+const helmet = require('helmet')
+const cors = require('cors')
+const xss = require('xss-clean')
+const rateLimiter = require('express-rate-limit')
+
 const jobs = require('./routes/jobs')
 const connectDB = require('./db/connect')
 
@@ -9,8 +16,17 @@ const app = express()
 
 const errorHandlerMiddleware = require('./middlewares/error-handler')
 
+// extra packages
+app.set('trust proxy', 1)
+app.use(rateLimiter({
+  window: 15*60* 1000, // 15 mins
+  max: 100, //limit each IP to 100 requests per window
+}))
 
 app.use(express.json())
+app.use(helmet())
+app.use(cors())
+app.use(xss())
 
 const port = process.env.PORT || 3000
 
