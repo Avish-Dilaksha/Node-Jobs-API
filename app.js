d7959d5165da11218f022e19e703e9eec2eea40e
Avish-Dilaksha <adilaksha@gmail.com>
2023-03-06 07:49:18 +0530

    Created app.js
    Updated app.js and checked the functionaliy using postman

--- app.js
+++ app.js
@@ -2,17 +2,25 @@ require('dotenv').config()
 require('express-async-errors')
 
 const express = require('express')
+const jobs = require('./routes/jobs')
+const connectDB = require('./db/connect')
 
 const app = express()
+
+const errorHandlerMiddleware = require('./middlewares/error-handler')
 
 
 app.use(express.json())
 
 const port = process.env.PORT || 3000
 
+app.use('/api/v1/jobs', jobs)
+
+app.use(errorHandlerMiddleware)
+
 const start = async () => {
     try {
-        //await connectDB(process.env.MONGO_URL)
+        await connectDB(process.env.MONGO_URL)
         app.listen(port, () => {
             console.log(`Server is listening on port ${port}...`);
         })
